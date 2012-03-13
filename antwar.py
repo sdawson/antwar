77eb3ec4de5bc8ca7a81d56638913436aacadb7a
Sophie Dawson <sophiecdawson@gmail.com>
2012-03-13 13:14:16 +1100

    Setting up cell check lists.

--- antwar.py
+++ antwar.py
@@ -19,6 +19,8 @@ def main():
   noOfSteps = int(sys.argv[4])
 
   printGrid(grid)
+  print genCheckList(grid, 5, 5, "nondiag")
+  print genCheckList(grid, 5, 5, "diag")
   sys.exit(0)
 
   for i in range(noOfSteps):
@@ -30,23 +32,103 @@ def initGrid(n):
 
 # Grid is updated destructively, so a new copy
 # of the grid is returned
-def updateGrid(grid, birthProb, majorProb):
+def updateGrid(grid, birthProb, majorProb, check="nodiag"):
   newGrid = grid.copy()
   (r, c) = grid.shape
   for i in range(r):
     for j in range(c):
       if grid[i][j] == EMPTY:
         newGrid[i][j] = maybePopulateCell(birthProb, majorProb)
-      elif grid[i][j] == REDMINOR or grid[i][j] == BLUEMINOR:
-        newGrid[i][j] = updateMinorCell(grid, i, j)
       else:
-        newGrid[i][j] = updateMajorCell(grid, i, j)
+        newGrid[i][j] = updateCell(grid, i, j, check)
 
-def updateMinorCell(grid, r, c):
-  pass
+def updateCell(grid, r, c, check):
+  if grid[r][c] == REDMINOR or grid[r][c] == BLUEMINOR:
+    if isDeathCondition(grid, r, c, check, 1, 1):
+      return EMPTY
+    else:
+      return grid[r][c]
+  elif grid[r][c] == REDMAJOR or grid[r][c] == BLUEMAJOR:
+    if isDeathCondition(grid, r, c, check, 4, 1):
+      return EMPTY
+    else:
+      return grid[r][c]
+  else:
+    print "Error: invalid cell value %d" % grid[r][c]
+    sys.exit(1)
 
-def updateMajorCell(grid, r, c):
-  pass
+def isDeathCondition(grid, r, c, check, noOfMinors, noOfMajors):
+  minorCount = 0
+  majorCount = 0
+  for (i, j) in genCheckList(grid, r, c, check):
+    if (grid[r][c] == BLUEMAJOR
+        or grid[r][c] == BLUEMINOR) and (grid[i][j] == REDMAJOR):
+      majorCount = majorCount + 1
+    elif (grid[r][c] == BLUEMAJOR
+          or grid[r][c] == BLUEMINOR) and (grid[i][j] == REDMINOR):
+      minorCount = minorCount + 1
+    elif (grid[r][c] == REDMAJOR
+          or grid[r][c] == REDMINOR) and (grid[i][j] == BLUEMAJOR):
+      majorCount = majorCount + 1
+    elif (grid[r][c] == REDMAJOR
+          or grid[r][c] == REDMINOR) and (grid[i][j] == BLUEMINOR):
+      minorCount = minorCount + 1
+  return minorCount >= noOfMinors or majorCount >= noOfMajors
+
+# generates a list of cells to check, based on
+# whether diagonal cells should be checked or not.
+def genCheckList(grid, r, c, check):
+  (maxr, maxc) = grid.shape
+  cells = []
+  if r == 0:
+    cells.append((r+1, c))
+    if c == 0:
+      cells.append((r, c+1))
+      if check == "diag":
+        cells.append((r+1, c+1))
+    elif c == maxc - 1:
+      cells.append((r, c-1))
+      if check == "diag":
+        cells.append((r+1, c-1))
+    else:
+      cells.extend([(r, c-1), (r, c+1)])
+      if check == "diag":
+        cells.extend([(r+1, c-1), (r+1, c+1)])
+  elif r == maxr - 1:
+    cells.append((r-1, c))
+    if c == 0:
+      cells.append((r, c+1))
+      if check == "diag":
+        cells.append((r-1, c+1))
+    elif c == maxc - 1:
+      cells.append((r, c-1))
+      if check == "diag":
+        cells.append((r-1, c-1))
+    else:
+      cells.extend([(r, c-1), (r, c+1)])
+      if check == "diag":
+        cells.extend([(r-1, c-1), (r-1, c+1)])
+  else:
+    if check == "diag":
+      cells.extend([(r, c-1), (r, c+1), (r-1, c), (r+1, c),
+        (r-1, c-1), (r-1, c+1), (r+1, c-1), (r+1, c+1)])
+    else:
+      cells.extend([(r, c-1), (r, c+1), (r-1, c), (r+1, c)])
+  return cells
+
+def addColCells(cells, r, c, offsetRow):
+  if c == 0:
+    cells.append((r, c+1))
+    if check == "diag":
+      cells.append((offsetRow, c+1))
+  elif c == maxc - 1:
+    cells.append((r, c-1))
+    if check == "diag":
+      cells.append((offsetRow, c-1))
+  else:
+    cells.extend([(r, c-1), (r, c+1)])
+    if check == "diag":
+      cells.extend([(offsetRow, c-1), (offsetRow, c+1)])
 
 def maybePopulateCell(birthProb, majorProb):
   birthRand = random.random()
@@ -75,7 +157,6 @@ def printGrid(grid):
     for j in range(c):
       print "%s " % cellPrinting(grid[i][j]), # comma suppresses newline
     print
-
 
 def usage():
   print "python antwar.py gridsize birthprob majorprob noofsteps"
