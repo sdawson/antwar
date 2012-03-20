ed59f21aacfd2edfb5fdf4a272011d856fdb2134
Sophie Dawson <sophiecdawson@gmail.com>
2012-03-20 18:34:11 +1100

    Added cross-platform script support.

--- runMany.sh
+++ runMany.sh
@@ -3,13 +3,22 @@
 
 filename="antsim-`date "+%Y%m%d%H%M"`.out"
 
+unamestr=`uname`
+
 touch $filename
 echo "10x10 grid, p = 0.03, f = 0.21, 1000 steps" >> $filename
 echo -e "Ma D\tMi D\ts+\ts-" >> $filename
 
-for i in `seq 1 1000`
+for i in `eval echo {1..1000}`
 do
-  echo "running simulation $i..."
-  python2 antwar.py 10 .03 .21 1000 >> $filename
+  if [ "$unamestr" = Linux ]
+  then
+    echo "running simulation $i..."
+    python2 antwar.py 10 .43 .02 1000 >> $filename
+  elif [ "$unamestr" = Darwin ]
+  then
+    echo "running simulation $i..."
+    python antwar.py 10 .43 .02 1000 >> $filename
+  fi
 done
 echo "Output in: $filename"
