5f914599d375a2b833dae4a089350538083f61b9
Sophie Dawson <sophiecdawson@gmail.com>
2012-03-20 18:35:57 +1100

    Initial test code for multi-threaded matrix update.

---
+++ multi.py
@@ -0,0 +1,16 @@
+import multiprocessing as mp
+
+import numpy
+
+ON = 1
+OFF = -1
+EMPTY = 0
+
+def main():
+  grid = numpy.zeros((5, 5), dtype=numpy.int)
+  print mp.cpu_count()
+
+jj
+
+if __name__ == "__main__":
+  main()
