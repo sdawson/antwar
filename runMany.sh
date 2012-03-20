c05f359acb867d47fe3b28cef72d64fe5a319309
Sophie Dawson <sophiecdawson@gmail.com>
2012-03-20 22:09:03 +1100

    Adding simulation settings in filename

--- runMany.sh
+++ runMany.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 # Run a large (1000?) number of ant simulations and write the results to a single file
 
-filename="antsim-`date "+%Y%m%d%H%M"`.out"
+p=0.43
+f=0.02
+steps=1000
+size=10
+filename="antsim-p$p-f$f-steps$steps-size$size-`date "+%Y%m%d%H%M"`.out"
 
 unamestr=`uname`
 
 touch $filename
-echo "10x10 grid, p = 0.43, f = 0.02, 1000 steps" >> $filename
+echo "10x10 grid, p = $p, f = $f, $steps steps" >> $filename
 echo -e "Ma D\tMi D\ts+\ts-" >> $filename
 
-for i in `eval echo {1..1000}`
+for i in `eval echo {1..1}`
 do
   if [ "$unamestr" = Linux ]
   then
     echo "running simulation $i..."
-    python2 antwar.py 10 .43 .02 1000 >> $filename
+    python2 antwar.py $size $p $f $steps >> $filename
   elif [ "$unamestr" = Darwin ]
   then
     echo "running simulation $i..."
-    python antwar.py 10 .43 .02 1000 >> $filename
+    python antwar.py $size $p $f $steps >> $filename
   fi
 done
 echo "Output in: $filename"
