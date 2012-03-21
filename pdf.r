2fd1ae9c164e16acecc8eacd1c442eac9b72488a
Sophie Dawson <sophiecdawson@gmail.com>
2012-03-22 00:29:50 +1100

    Set up different graphing for multi/single major ant prob.

--- pdf.r
+++ pdf.r
@@ -1,10 +1,15 @@
 # Clear variables
 rm(list=ls())
 
-graph_file <- function(infile, outfile) {
+graph_file <- function(infile, outfile, isMultipleMajor) {
   graph_par <- par(las = 0, mar = c(4, 4, 3, 2))
-  file_contents <- read.table(infile, skip=2, sep="\t", col.names=c("mad",
-    "mid", "redd", "blued", "sma", "smi"), dec=".")
+  col_names <- NULL
+  if (isMultipleMajor) {
+    col_names <- c("mad", "mid", "redd", "blued", "redsma", "bluesma", "smi")
+  } else {
+    col_names <- c("mad", "mid", "redd", "blued", "sma", "smi")
+  }
+  file_contents <- read.table(infile, skip=2, sep="\t", col.names=col_names, dec=".")
 
   pdf(file = outfile)
   # Plotting minor ant pdf
@@ -12,14 +17,34 @@ graph_file <- function(infile, outfile) {
   axis(1)
   axis(2)
   box()
-  title(main="Minor Ant Death PDF", xlab="Minor ant deaths per time step",
+  title(main="Minor Ant Deaths", xlab="Minor ant deaths per time step",
         ylab="Density")
 
   # plotting major ant pdf
-  plot(density(file_contents$sma), axes=FALSE, xlab="Major ant deaths per time 
-       step", ylab="Density", main="Major Ant Death PDF")
-  axis(1)
-  axis(2)
-  box()
+  if (isMultipleMajor) {
+    plot(density(file_contents$redsma), axes=FALSE, xlab="Red Major ant deaths
+                 per time step", ylab="Density", main="Red Major Ant Deaths")
+    axis(1)
+    axis(2)
+    box()
+    plot(density(file_contents$bluesma), axes=FALSE, xlab="Blue Major ant deaths
+                 per time step", ylab="Density", main="Blue Major Ant Deaths")
+    axis(1)
+    axis(2)
+    box()
+  } else {
+    plot(density(file_contents$sma), axes=FALSE, xlab="Major ant deaths per time
+         step", ylab="Density", main="Major Ant Deaths")
+    axis(1)
+    axis(2)
+    box()
+  }
   dev.off()
 }
+
+args <- commandArgs(trailingOnly = TRUE)
+infile <- args[1]
+outfile <- args[2]
+isMultipleMajor <- as.logical(args[3])
+
+graph_file(infile, outfile, isMultipleMajor)
