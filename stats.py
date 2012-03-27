9366beb31ecc0f25b7ff0c0310f0e459a514fa82
Sophie Dawson <sophiecdawson@gmail.com>
2012-03-27 22:31:14 +1100

    Added state write to file functionality.

--- stats.py
+++ stats.py
@@ -4,15 +4,35 @@ class AntStats(object):
     self.tribes = tribes
     # A list of number of deaths per step, one list per tribe
     self.majorDeaths = [[0]*size**2]*len(tribes)
-    print self.majorDeaths
     self.minorDeaths = [[0]*size**2]*len(tribes)
-    print self.minorDeaths
 
   # Takes lists of major and minor ant death counts
   # for each tribe, and updates the total counts
   def updateStats(self, majorDeaths, minorDeaths):
-    for tribeId in range(0, len(self.tribes)):
+    for (tribeId, tribe) in enumerate(self.tribes):
       self.majorDeaths[tribeId][majorDeaths[tribeId]] = \
           self.majorDeaths[tribeId][majorDeaths[tribeId]] + 1
       self.minorDeaths[tribeId][minorDeaths[tribeId]] = \
           self.minorDeaths[tribeId][minorDeaths[tribeId]] + 1
+
+  def writeToFile(self, filename):
+    f = open(filename, "w")
+    titles = self._createColTitles()
+    f.write(titles)
+    for i in range(len(self.majorDeaths[0])):
+      f.write("%d\t" % i)
+      for (tn, t) in enumerate(self.tribes):
+        if tn == 0:
+          f.write("%d\t%d" % (self.majorDeaths[tn][i], self.minorDeaths[tn][i]))
+        else:
+          f.write("\t%d\t%d" % (self.majorDeaths[tn][i], self.minorDeaths[tn][i]))
+      f.write("\n")
+    f.close()
+
+
+  def _createColTitles(self):
+    titles = ["NoOfDeaths"]
+    for tribe in self.tribes:
+      titles.extend([tribe + "MajDeath", tribe + "MinDeath"])
+    titles.append("\n")
+    return "\t".join(titles)
