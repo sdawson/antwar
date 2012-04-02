bdf56f6ae541d06d7f9d2e889fa8f29acb4b5eb8
Sophie Dawson <sophiecdawson@gmail.com>
2012-04-02 16:17:57 +1000

    Adding trailing zero removal from stats arrays.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import grid, os, time
 
 def main():
-  initGrid = grid.Grid(30, ["red", "blue"], 0.04, 0.12, True)
+  initGrid = grid.Grid(30, ["red", "blue"], 0.10, 0.05, True)
   #initGrid = grid.Grid(30, ["red", "blue"], 0.5, 0.22, True)
   for i in range(2000):
     initGrid.updateGrid()
@@ -9,8 +9,7 @@ def main():
       time.sleep(.5)
       os.system('cls' if os.name == 'nt' else 'clear')
       initGrid.printGrid()
-      print i
-  initGrid.antStats.writeToFile("2teams-2000steps-0.04p-0.12f-diag")
+  initGrid.antStats.writeToFile("2teams-2000steps-0.10p-0.05f-diag-withcapping")
 
 if __name__ == "__main__":
   main()
--- stats.py
+++ stats.py
@@ -16,22 +16,39 @@ class AntStats(object):
           self.minorDeaths[tribeId][minorDeaths[tribeId]] + 1
 
   def writeToFile(self, filename):
+    lastRow = self._stripTrailingZeros()
     f = open(filename, "w")
     titles = self._createColTitles()
     f.write(titles)
-    for i in range(len(self.majorDeaths[0])):
+    for i in range(lastRow+1):
       f.write("%d\t" % i)
       for (tn, t) in enumerate(self.tribes):
         if tn == 0:
-          f.write("%d\t%d" % (self.majorDeaths[tn][i], self.minorDeaths[tn][i]))
+         f.write("%d\t%d" % (self.majorDeaths[tn][i], self.minorDeaths[tn][i]))
         else:
           f.write("\t%d\t%d" % (self.majorDeaths[tn][i], self.minorDeaths[tn][i]))
       f.write("\n")
     f.close()
-
 
   def _createColTitles(self):
     titles = ["NoOfDeaths"]
     for tribe in self.tribes:
       titles.extend([tribe + "MajDeath", tribe + "MinDeath"])
     return "\t".join(titles) + "\n"
+
+  # Reading from the end of the death count lists, determines the
+  # last non-zero row and returns its index.
+  def _stripTrailingZeros(self):
+    end = len(self.majorDeaths[0])
+    print "len:", end
+    zeroRow = end-1
+    rowCounter = 0
+    for i in range(end-1, -1, -1):
+      rowCounter = 0
+      for (tribeId, _) in enumerate(self.tribes):
+        rowCounter += self.majorDeaths[tribeId][i] + self.minorDeaths[tribeId][i]
+      if rowCounter == 0:
+        zeroRow = i
+      else:
+        # Have hit the first non-zero row from the end
+        return zeroRow
